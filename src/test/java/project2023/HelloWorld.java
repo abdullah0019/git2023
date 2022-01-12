b426deddb6b403deb6eaf2bce8831d637fc5a609
abdullah0019 <80920892+abdullah0019@users.noreply.github.com>
2022-01-11 22:39:46 -0500

    Update HelloWorld.java
    delete again

--- src/test/java/project2023/HelloWorld.java
+++ src/test/java/project2023/HelloWorld.java
@@ -3,7 +3,7 @@ package project2023;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Raihan bhai what");
-        System.out.println("Raihan bhai weldone");
+   
 
     }
 }
